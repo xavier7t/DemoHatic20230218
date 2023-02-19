43ba82dc66d74d9dbe32a2e5ed9c9cf8c42d735b
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-18 22:26:32 -0500

    Initial Commit

---
+++ DemoHatic20230218/DemoHatic20230218App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoHatic20230218App.swift
+//  DemoHatic20230218
+//
+//  Created by Xavier on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoHatic20230218App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
