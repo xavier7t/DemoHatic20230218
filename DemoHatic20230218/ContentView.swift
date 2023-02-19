53f93b9cbe1382d92741e39d5a49951e8f200375
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-18 23:14:09 -0500

    DemoHatic20230218

--- DemoHatic20230218/ContentView.swift
+++ DemoHatic20230218/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button("Tap me") {
+//                let generator = UINotificationFeedbackGenerator()
+//                generator.notificationOccurred(.error)
+                let generator = UIImpactFeedbackGenerator(style: .heavy)
+                generator.impactOccurred()
+            }
+            
         }
         .padding()
     }
